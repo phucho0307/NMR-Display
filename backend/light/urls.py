316da79a27f678f1fae803807d1862e491c262a1
phuc t ho <751920hotanphuc@gmail.com>
2026-02-15 06:15:32 +0700

    updated all image references from /images/... to /static/images/ and added static file serving to urls.py

--- backend/light/urls.py
+++ backend/light/urls.py
@@ -18,8 +18,14 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, re_path
 from django.views.generic import TemplateView
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
-]   
+]
+
+# Serve static files in development
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()   
